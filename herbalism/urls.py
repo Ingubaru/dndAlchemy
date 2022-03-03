9be1b5580d6d5d1752143127ecefb3e07a249949
Ingubaru <neveroff92@gmail.com>
2022-03-04 00:23:15 +0600

    Add generate menu

--- herbalism/urls.py
+++ herbalism/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
 	path('', views.ingredient_list, name='ingredient_list'),
+	path('generator_menu', views.generator_menu, name='generator_menu'),
 	re_path(r'^ingredient/(?P<pk>[0-9]+)/$', views.ingredient_detail, name="ingredient_detail"),
 ]
--- herbalism/views.py
+++ herbalism/views.py
@@ -10,3 +10,7 @@ def ingredient_list(request):
 def ingredient_detail(request, pk):
 	ingredient = get_object_or_404(Ingredient, pk=pk)
 	return render(request, 'herbalism/ingredient_detail.html', {'ingredient': ingredient})
+
+
+def generator_menu(request):
+	return render(request, 'herbalism/generator_menu.html')
