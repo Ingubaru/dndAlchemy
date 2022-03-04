7998bb15dafe6abab442aa66e7b2aa8985042384
Ingubaru <neveroff92@gmail.com>
2022-03-04 17:51:45 +0600

    Add ingredient card

--- herbalism/urls.py
+++ herbalism/urls.py
@@ -2,7 +2,8 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-	path('', views.ingredient_list, name='ingredient_list'),
+	path('', views.generator_menu, name='generator_menu'),
+	path('ingredient_list', views.ingredient_list, name='ingredient_list'),
 	path('generator_menu', views.generator_menu, name='generator_menu'),
 	re_path(r'^ingredient/(?P<pk>[0-9]+)/$', views.ingredient_detail, name="ingredient_detail"),
 ]
